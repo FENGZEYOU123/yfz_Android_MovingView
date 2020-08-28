6577685b3448e52fd2936711e119727d02d59c91
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2020-08-28 11:39:44 +0800

    1

--- Main/app/src/main/java/com/example/main_movingview/MainActivity.java
+++ Main/app/src/main/java/com/example/main_movingview/MainActivity.java
@@ -10,21 +10,11 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
-    private yfz_ios_spring_listview listview;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);  //去掉标题栏
         setContentView(R.layout.activity_main);
         //移动MovingView的代码 在MovingView.java 里
-
-        listview = (yfz_ios_spring_listview) findViewById(R.id.mylistview);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, new String[] { "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"});
-        listview.setAdapter(adapter);
-
-
-
     }
 }
